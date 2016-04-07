e77406f7169f8eee7be85b676e3ab560dcc824e2
Junxue Zhang <jxzhang@seu.edu.cn>
2016-04-07 22:37:08 +0800

    Add lb examples

--- examples/load-balance/dctcp-drb-original.cc
+++ examples/load-balance/dctcp-drb-original.cc
@@ -4,11 +4,119 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/traffic-control-module.h"
 #include "ns3/ipv4-drb-helper.h"
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("DcTcpDrbOriginal");
+
+class MyApp : public Application
+{
+public:
+
+  MyApp ();
+  virtual ~MyApp();
+
+  void Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate);
+
+private:
+  virtual void StartApplication (void);
+  virtual void StopApplication (void);
+
+  void ScheduleTx (void);
+  void SendPacket (void);
+
+  Ptr<Socket>     m_socket;
+  Address         m_peer;
+  uint32_t        m_packetSize;
+  uint32_t        m_nPackets;
+  DataRate        m_dataRate;
+  EventId         m_sendEvent;
+  bool            m_running;
+  uint32_t        m_packetsSent;
+};
+
+MyApp::MyApp ()
+  : m_socket (0),
+    m_peer (),
+    m_packetSize (0),
+    m_nPackets (0),
+    m_dataRate (0),
+    m_sendEvent (),
+    m_running (false),
+    m_packetsSent (0)
+{
+}
+
+MyApp::~MyApp()
+{
+  m_socket = 0;
+}
+
+void
+MyApp::Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate)
+{
+  m_socket = socket;
+  m_peer = address;
+  m_packetSize = packetSize;
+  m_nPackets = nPackets;
+  m_dataRate = dataRate;
+}
+
+void
+MyApp::StartApplication (void)
+{
+  m_running = true;
+  m_packetsSent = 0;
+  m_socket->Bind ();
+  m_socket->Connect (m_peer);
+  SendPacket ();
+}
+
+void
+MyApp::StopApplication (void)
+{
+  m_running = false;
+
+  if (m_sendEvent.IsRunning ())
+    {
+      Simulator::Cancel (m_sendEvent);
+    }
+
+  if (m_socket)
+    {
+      m_socket->Close ();
+    }
+}
+
+void
+MyApp::SendPacket (void)
+{
+  Ptr<Packet> packet = Create<Packet> (m_packetSize);
+  m_socket->Send (packet);
+
+  if (++m_packetsSent < m_nPackets)
+    {
+      ScheduleTx ();
+    }
+}
+
+void
+MyApp::ScheduleTx (void)
+{
+  if (m_running)
+    {
+      Time tNext (Seconds (m_packetSize * 8 / static_cast<double> (m_dataRate.GetBitRate ())));
+      m_sendEvent = Simulator::Schedule (tNext, &MyApp::SendPacket, this);
+    }
+}
+
+static void
+CwndChange (uint32_t oldCwnd, uint32_t newCwnd)
+{
+    NS_LOG_UNCOND (Simulator::Now ().GetSeconds () << "\t" << newCwnd);
+}
 
 int main (int argc, char *argv[])
 {
@@ -42,26 +150,39 @@ int main (int argc, char *argv[])
 
     NS_LOG_INFO ("Install channel");
     PointToPointHelper p2p;
+    p2p.SetQueue ("ns3::DropTailQueue");
     p2p.SetDeviceAttribute ("DataRate", StringValue ("10Gbps"));
     p2p.SetChannelAttribute ("Delay", TimeValue (MicroSeconds(100)));
 
     NetDeviceContainer d0d1 = p2p.Install (n0n1);
 
-    p2p.SetQueue ("ns3::RedQueueDisc", "Mode", StringValue("QUEUE_MODE_PACKETS"),
-                                       "MinTh", DoubleValue(65),
-                                       "MaxTh", DoubleValue(65),
-                                       "QueueLimit", UintegerValue(1000));
+    Config::SetDefault ("ns3::RedQueueDisc::Mode", StringValue ("QUEUE_MODE_PACKETS"));
+    Config::SetDefault ("ns3::RedQueueDisc::QueueLimit", UintegerValue (1000));
+
+    TrafficControlHelper tchRed;
+    tchRed.SetRootQueueDisc ("ns3::RedQueueDisc", "LinkBandwidth", StringValue ("10Gbps"),
+                                                  "LinkDelay", TimeValue (MicroSeconds (100)),
+                                                  "MinTh", DoubleValue (65),
+                                                  "MaxTh", DoubleValue (65));
     NetDeviceContainer d1d2 = p2p.Install (n1n2);
+    tchRed.Install (d1d2);
     NetDeviceContainer d2d4 = p2p.Install (n2n4);
+    tchRed.Install (d2d4);
     NetDeviceContainer d4d5 = p2p.Install (n4n5);
+    tchRed.Install (d4d5);
 
     p2p.SetDeviceAttribute ("DataRate", StringValue ("1Gbps"));
-    p2p.SetQueue ("ns3::RedQueueDisc", "Mode", StringValue("QUEUE_MODE_PACKETS"),
-                                       "MinTh", DoubleValue(20),
-                                       "MaxTh", DoubleValue(20),
-                                       "QueueLimit", UintegerValue(1000));
+
+    TrafficControlHelper tchRed2;
+    tchRed2.SetRootQueueDisc ("ns3::RedQueueDisc", "LinkBandwidth", StringValue ("1Gbps"),
+                                                  "LinkDelay", TimeValue (MicroSeconds (100)),
+                                                  "MinTh", DoubleValue (20),
+                                                  "MaxTh", DoubleValue (20));
+
     NetDeviceContainer d1d3 = p2p.Install (n1n3);
+    tchRed2.Install (d1d3);
     NetDeviceContainer d3d4 = p2p.Install (n3n4);
+    tchRed2.Install (d3d4);
 
     NS_LOG_INFO ("Assign IP address");
     Ipv4AddressHelper ipv4;
@@ -92,6 +213,26 @@ int main (int argc, char *argv[])
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
+    NS_LOG_INFO ("Install TCP based application");
+    uint16_t port = 8080;
+    Address sinkAddress (InetSocketAddress (i4i5.GetAddress(1), port));
+    Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (c.Get(0), TcpSocketFactory::GetTypeId ());
+    ns3TcpSocket->TraceConnectWithoutContext("CongestionWindow", MakeCallback (&CwndChange));
+
+    Ptr<MyApp> app = CreateObject<MyApp> ();
+    app->Setup (ns3TcpSocket, sinkAddress, 1040, 1000000, DataRate ("10Gbps"));
+    c.Get(0)->AddApplication (app);
+    app->SetStartTime (Seconds (1.0));
+    app->SetStopTime (Seconds (10.0));
+
+    PacketSinkHelper sink ("ns3::TcpSocketFactory",
+            InetSocketAddress (Ipv4Address::GetAny (), port));
+    ApplicationContainer sinkApp = sink.Install (c.Get(5));
+    sinkApp.Start (Seconds (0.1));
+    sinkApp.Stop (Seconds (10.0));
+
+    Simulator::Run ();
+    Simulator::Destroy ();
 
     return 0;
 }
