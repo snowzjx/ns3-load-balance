dc703d6d77269e6728555c58acb460009cc2df8e
Junxue Zhang <jxzhang@seu.edu.cn>
2016-06-27 18:29:41 +0800

    Finish part of the Conga control loop
    Should be checked before using in real simulations

--- src/internet/model/ipv4-conga.cc
+++ src/internet/model/ipv4-conga.cc
@@ -1,6 +1,12 @@
 #include "ns3/log.h"
+#include "ns3/simulator.h"
 #include "ipv4-conga.h"
 #include "ipv4-conga-tag.h"
+#include "ns3/flow-id-tag.h"
+
+#include <limits>
+#include <time.h>
+#include <algorithm>
 
 namespace ns3
 {
@@ -21,14 +27,35 @@ Ipv4Conga::GetTypeId (void)
 }
 
 Ipv4Conga::Ipv4Conga ():
-    m_isLeaf (false), m_leafId (0)
+    m_isLeaf (false),
+    m_leafId (0),
+    m_X (0)
 {
   NS_LOG_FUNCTION (this);
+  srand((unsigned)time(NULL));
 }
 
 Ipv4Conga::~Ipv4Conga ()
 {
   NS_LOG_FUNCTION (this);
+  std::map<uint32_t, Flowlet *>::iterator itr = m_flowletTable.begin ();
+  for ( ; itr != m_flowletTable.end (); ++itr )
+  {
+    delete (itr->second);
+  }
+
+  std::map<uint32_t, std::vector<FeedbackInfo*> >::iterator fbItr = m_congaFromLeafTable.begin ();
+  for ( ; fbItr != m_congaFromLeafTable.end (); ++fbItr )
+  {
+    std::vector<FeedbackInfo *>::iterator fbItemItr = (fbItr->second).begin ();
+    for ( ; fbItemItr != (fbItr->second).end (); ++fbItemItr )
+    {
+      if ((*fbItemItr) != NULL)
+      {
+        delete (*fbItemItr);
+      }
+    }
+  }
 }
 
 void
@@ -39,14 +66,34 @@ Ipv4Conga::SetLeafId (uint32_t leafId)
 }
 
 void
+Ipv4Conga::SetFlowletTimeout (double timeout)
+{
+  m_flowletTimeout = timeout;
+}
+
+void
 Ipv4Conga::AddAddressToLeafIdMap (Ipv4Address addr, uint32_t leafId)
 {
   m_ipLeafIdMap[addr] = leafId;
 }
 
-uint32_t
-Ipv4Conga::ProcessPacket (Ptr<Packet> packet)
+bool
+Ipv4Conga::ProcessPacket (Ptr<Packet> packet, Ipv4Header ipv4Header, uint32_t &path, uint32_t availPath)
 {
+  // Packet arrival time
+  double now = Simulator::Now ().GetSeconds ();
+
+  // Extract the flow id
+  uint32_t flowId = 0;
+  FlowIdTag flowIdTag;
+  bool flowIdFound = packet->PeekPacketTag(flowIdTag);
+  if (!flowIdFound)
+  {
+    NS_LOG_ERROR ("Ipv4Conga::ProcessPacket Cannot find the flow id");
+    return false;
+  }
+  flowId = flowIdTag.GetFlowId ();
+
   // First, check if this switch if leaf switch
   if (m_isLeaf)
   {
@@ -54,23 +101,212 @@ Ipv4Conga::ProcessPacket (Ptr<Packet> packet)
     // 1. The sender is connected to this leaf switch
     // 2. The receiver is connected to this leaf switch
     // We can distinguish it by checking whether the packet has CongaTag
-    Ipv4CongaTag ipv4CongTag;
-    bool found = packet->PeekPacketTag(ipv4CongTag);
+    Ipv4CongaTag ipv4CongaTag;
+    bool found = packet->PeekPacketTag(ipv4CongaTag);
 
     if (!found)
     {
-      // Sending a new packet
+      // When sending a new packet
+      // Build an empty Conga header (as the packet tag)
+      // Determine the path and fill the header fields
+
+      // Determine the dest switch leaf id
+      std::map<Ipv4Address, uint32_t>::iterator itr = m_ipLeafIdMap.find(ipv4Header.GetDestination ());
+      if (itr == m_ipLeafIdMap.end ())
+      {
+        NS_LOG_ERROR ("Ipv4Conga::ProcessPacket Cannot find leaf switch id");
+        return false;
+      }
+      uint32_t destLeafId = itr->second;
+
+      // TODO Check piggyback information
+      std::map<uint32_t, std::vector<FeedbackInfo*> >::iterator fbItr =
+          m_congaFromLeafTable.find (destLeafId);
+
+      uint8_t fbLbTag = 0;
+      uint8_t fbMetric = 0;
+
+      // Piggyback the last changed one
+      if (fbItr != m_congaFromLeafTable.end ())
+      {
+        uint32_t maxChange = 0;
+        for (uint32_t pathId = 0; pathId < (fbItr->second).size (); pathId ++)
+        {
+            if ((fbItr->second)[pathId]->change > maxChange)
+          {
+            fbLbTag = pathId;
+            fbMetric = (fbItr->second)[pathId]->ce;
+            maxChange = (fbItr->second)[pathId]->change;
+          }
+        }
+        // Update the Conga From Leaf Table
+        (fbItr->second)[fbLbTag]->change = 0;
+      }
+
+      // Path determination logic:
+      // Firstly, check the flowlet table to see whether there is existing flowlet
+      // If not hit, determine the path based on the congestion degree of the link
+
+      // Flowlet table look up
+      struct Flowlet *flowlet = NULL;
+
+      // If the flowlet table entry is valid, return the path
+      std::map<uint32_t, struct Flowlet *>::iterator flowletItr = m_flowletTable.find (flowId);
+      if (flowletItr != m_flowletTable.end ())
+      {
+        flowlet = flowletItr->second;
+        if (flowlet != NULL && // Impossible in normal cases
+            now - flowlet->activeTime <= m_flowletTimeout)
+        {
+          // Do not forget to update the flowlet active time
+          flowlet->activeTime = now;
+
+          // Return the path information used for routing routine to select the path
+          path = flowlet->pathId;
+
+          // Construct Conga Header for the packet
+          ipv4CongaTag.SetLbTag (path);
+          ipv4CongaTag.SetCe (0);
+
+          // TODO Piggyback the feedback information
+          ipv4CongaTag.SetFbLbTag (fbLbTag);
+          ipv4CongaTag.SetFbMetric (fbMetric);
+          packet->AddPacketTag(ipv4CongaTag);
+
+          return true;
+        }
+      }
+
+      // Not hit. Determine the path
+
+      // 1. Select path congestion information based on dest leaf switch id
+      std::map<uint32_t, std::vector<double> >::iterator pathInfoItr = m_congaToLeafTable.find (destLeafId);
+
+      std::vector<double> pathCongestions;
+
+      // Initialize Conga To Leaf Table if no entry is found
+      if (pathInfoItr == m_congaToLeafTable.end ())
+      {
+        for (uint32_t pathId = 0; pathId < availPath; pathId ++)
+        {
+          pathCongestions[pathId] = 0.0f;
+        }
+        m_congaToLeafTable[destLeafId] = pathCongestions;
+      }
+      else
+      {
+        pathCongestions = pathInfoItr->second;
+      }
+
+      // 2. Prepare the candidate path
+      double minPathCongestion = (std::numeric_limits<double>::max)();
+      std::vector<uint32_t> pathCandidates;
+      for (uint32_t pathId = 0; pathId < pathCongestions.size(); pathId ++)
+      {
+        if (pathCongestions[pathId] < minPathCongestion)
+        {
+          // Strictly better path
+          minPathCongestion = pathCongestions[pathId];
+          pathCandidates.clear();
+          pathCandidates.push_back(pathId);
+        }
+        if (pathCongestions[pathId] == minPathCongestion)
+        {
+          // Equally good path
+          pathCandidates.push_back(pathId);
+        }
+      }
+
+      // 3. Select one path from all those candidate paths
+      if (flowlet != NULL &&
+            std::find(pathCandidates.begin (), pathCandidates.end (), flowlet->pathId) != pathCandidates.end ())
+      {
+        // Prefer the path cached in flowlet table
+        path = flowlet->pathId;
+        // Activate the flowlet entry again
+        flowlet->activeTime = now;
+      }
+      else
+      {
+        // If there are no cached paths, we randomly choose a good path
+        path = rand() % availPath;
+        if (flowlet == NULL)
+        {
+          struct Flowlet *newFlowlet = new Flowlet;
+          newFlowlet->pathId = path;
+          newFlowlet->activeTime = now;
+          m_flowletTable[flowId] = newFlowlet;
+        }
+        else
+        {
+          flowlet->pathId = path;
+          flowlet->activeTime = now;
+        }
+      }
+
+      // 4. Construct Conga Header for the packet
+      ipv4CongaTag.SetLbTag (path);
+      ipv4CongaTag.SetCe (0);
+
+      // TODO Piggyback the feedback information
+      ipv4CongaTag.SetFbLbTag (fbLbTag);
+      ipv4CongaTag.SetFbMetric (fbMetric);
+      packet->AddPacketTag(ipv4CongaTag);
+
+      return true;
     }
     else
     {
       // Forwarding the packet to dest
+
+      // Determine the source switch leaf id
+      std::map<Ipv4Address, uint32_t>::iterator itr = m_ipLeafIdMap.find(ipv4Header.GetSource ());
+      if (itr == m_ipLeafIdMap.end ())
+      {
+        NS_LOG_ERROR ("Ipv4Conga::ProcessPacket Cannot find leaf switch id");
+        return false;
+      }
+      uint32_t sourceLeafId = itr->second;
+
+      // 1. Update the CongaFromLeafTable
+      std::map<uint32_t, std::vector<FeedbackInfo*> >::iterator fromLeafItr = m_congaFromLeafTable.find (sourceLeafId);
+      if (fromLeafItr == m_congaFromLeafTable.end () ||
+            (fromLeafItr->second)[ipv4CongaTag.GetLbTag ()] == NULL) // No entry
+      {
+        FeedbackInfo *feedbackInfo = new FeedbackInfo ();
+        feedbackInfo->ce = ipv4CongaTag.GetCe ();
+        feedbackInfo->change = 1;
+        (fromLeafItr->second)[ipv4CongaTag.GetLbTag ()] = feedbackInfo;
+      }
+      else
+      {
+        (fromLeafItr->second)[ipv4CongaTag.GetLbTag ()]->ce = ipv4CongaTag.GetCe ();
+        (fromLeafItr->second)[ipv4CongaTag.GetLbTag ()]->change++;
+      }
+
+      // 2. Update the CongaToLeafTable
+      std::map<uint32_t, std::vector<double> >::iterator toLeafItr = m_congaToLeafTable.find(sourceLeafId);
+      if (toLeafItr != m_congaToLeafTable.end ())
+      {
+        (toLeafItr->second)[ipv4CongaTag.GetFbLbTag()] = ipv4CongaTag.GetFbMetric ();
+      }
     }
 
+    // Not necessary
+    // Remove the Conga Header
+    packet->RemovePacketTag (ipv4CongaTag);
+
+    // Pick path using standard ECMP
+    path = flowId % availPath;
+
+    return true;
   }
   else
   {
+    // If the switch is not leaf swith, DRE algorithm works here
+    //
+    return true;
   }
-  return 0;
 }
 
 }
--- src/internet/model/ipv4-conga.h
+++ src/internet/model/ipv4-conga.h
@@ -5,14 +5,20 @@
 #include <vector>
 #include "ns3/object.h"
 #include "ns3/packet.h"
-#include "ns3/ipv4-address.h"
+#include "ns3/ipv4-header.h"
 
 namespace ns3
 {
 
-struct PathCongestion {
+struct Flowlet {
   uint32_t pathId;
+  /* bool isValid; */
+  double activeTime;
+};
+
+struct FeedbackInfo {
   uint8_t ce;
+  uint32_t change;
 };
 
 class Ipv4Conga : public Object
@@ -25,9 +31,20 @@ public:
   ~Ipv4Conga ();
 
   void SetLeafId (uint32_t leafId);
+
+  void SetFlowletTimeout (double timeout);
+
   void AddAddressToLeafIdMap (Ipv4Address addr, uint32_t leafId);
 
-  uint32_t ProcessPacket (Ptr<Packet> packet);
+  /**
+   * \brief Conga packet processing routine, each packet should go through this routine
+   * \param packet The packet to process
+   * \param ipv4Header The ipv4 header
+   * \param path The path id, the path id should < availPath
+   * \param availPath The number of all available paths
+   * \return True if the routine is corrected executed, the path should be referred for the correct path.
+   */
+  bool ProcessPacket (Ptr<Packet> packet, Ipv4Header ipv4Header, uint32_t &path, uint32_t availPath);
 
 private:
   // Used to determine whether this switch is leaf switch
@@ -39,12 +56,20 @@ private:
   std::map<Ipv4Address, uint32_t> m_ipLeafIdMap;
 
   // Congestion to Leaf Table
-  std::map<uint32_t, std::vector<struct PathCongestion> > m_congToLeafTable;
+  std::map<uint32_t, std::vector<double> > m_congaToLeafTable;
 
   // Congestion From Leaf Table
-  std::map<uint32_t, std::vector<struct PathCongestion> > m_congFromLeafTable;
+  std::map<uint32_t, std::vector<FeedbackInfo *> > m_congaFromLeafTable;
 
+  // Flowlet Table
+  std::map<uint32_t, Flowlet *> m_flowletTable;
+
+  // Parameters
+  // DRE
   uint8_t m_X;
+
+  // Flowlet Timeout
+  double m_flowletTimeout;
 };
 
 }
