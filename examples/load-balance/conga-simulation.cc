0997b8aa39d5952f851308511d00c90d054f39a3
Junxue Zhang <jxzhang@seu.edu.cn>
2016-07-17 19:20:19 +0800

    Update simulation settings

--- examples/load-balance/conga-simulation.cc
+++ examples/load-balance/conga-simulation.cc
@@ -5,38 +5,33 @@
 #include "ns3/applications-module.h"
 #include "ns3/flow-monitor-module.h"
 #include "ns3/ipv4-conga-helper.h"
+#include "ns3/ipv4-drb-helper.h"
 
 #include <vector>
 
-// There are 32 servers connecting to each leaf switch
+// The CDF in TrafficGenerator
+extern "C"
+{
+#include "cdf.h"
+}
+
+// There are 8 servers connecting to each leaf switch
 #define LEAF_NODE_COUNT 8
 
+#define SPINE_LEAF_CAPACITY  4000000000           // 4Gbps
+#define LEAF_SERVER_CAPACITY 4000000000           // 4Gbps
+#define LINK_LATENCY MicroSeconds(100)            // 100 MicroSeconds
+#define BUFFER_SIZE 100                           // 100 Packets
+
 // The simulation starting and ending time
-#define START_TIME 0.1
-#define END_TIME 2.0
+#define START_TIME 0.0
+#define END_TIME 0.5
 
 // The flow port range, each flow will be assigned a random port number within this range
 #define PORT_START 2333
 #define PORT_END 6666
 
-// Request rate per second
-#define REQUEST_RATE 2
-
-// Flow configuration
-//
-//       +--------------------+--------------------+--------------------+
-//       |   Small flow       |   Medium flow      |    Large flow      |
-//       +--------------------+--------------------+--------------------+
-//
-// Size: 0            small_flow_max        large_flow_min      large_flow_max
-// Poss:  small_flow_poss  1-small_flow_poss-large_flow_poss  large_flow_poss
-
-#define SMALL_FLOW_MAX 100000       // 100KB
-#define LARGE_FLOW_MIN 10000000     // 10MB
-#define LARGE_FLOW_MAX 100000000    // 100MB
-
-#define SMALL_FLOW_POSS 0.5         // 50%
-#define LARGE_FLOW_POSS 0.2         // 20%
+#define PRESTO_RATIO 64
 
 using namespace ns3;
 
@@ -45,6 +40,7 @@ NS_LOG_COMPONENT_DEFINE ("CongaSimulation");
 enum RunMode {
     CONGA,
     CONGA_FLOW,
+    PRESTO,
     ECMP
 };
 
@@ -64,11 +60,12 @@ T rand_range (T min, T max)
     return min + ((double)max - min) * rand () / RAND_MAX;
 }
 
-void install_applications (NodeContainer fromServers, NodeContainer destServers, std::vector<Ipv4Address> toAddresses)
+void install_applications (NodeContainer fromServers, NodeContainer destServers, double requestRate,
+        struct cdf_table *cdfTable, std::vector<Ipv4Address> toAddresses)
 {
     for (int i = 0; i < LEAF_NODE_COUNT; i++)
     {
-        double startTime = START_TIME + poission_gen_interval (REQUEST_RATE);
+        double startTime = START_TIME + poission_gen_interval (requestRate);
         while (startTime < END_TIME)
         {
             uint16_t port = rand_range (PORT_START, PORT_END);
@@ -76,24 +73,8 @@ void install_applications (NodeContainer fromServers, NodeContainer destServers,
             BulkSendHelper source ("ns3::TcpSocketFactory",
                     InetSocketAddress (toAddresses[destIndex], port));
 
-            uint32_t flowSize = 0;
+            uint32_t flowSize = gen_random_cdf (cdfTable);
 
-            double randomValue = rand_range (0.0, 1.0);
-            if (randomValue <= SMALL_FLOW_POSS)
-            {
-                // small flow
-                flowSize = rand_range (0, SMALL_FLOW_MAX);
-            }
-            else if (randomValue > SMALL_FLOW_POSS && randomValue <= 1 - LARGE_FLOW_POSS)
-            {
-                // medium flow
-                flowSize = rand_range (SMALL_FLOW_MAX, LARGE_FLOW_MIN);
-            }
-            else
-            {
-                // large flow
-                flowSize = rand_range (LARGE_FLOW_MIN, LARGE_FLOW_MAX);
-            }
             source.SetAttribute ("MaxBytes", UintegerValue(flowSize));
 
             // Install apps
@@ -102,14 +83,13 @@ void install_applications (NodeContainer fromServers, NodeContainer destServers,
             sourceApp.Stop (Seconds (END_TIME));
 
             // Install packet sinks
-
             PacketSinkHelper sink ("ns3::TcpSocketFactory",
                     InetSocketAddress (Ipv4Address::GetAny (), port));
             ApplicationContainer sinkApp = sink.Install (destServers. Get (destIndex));
             sinkApp.Start (Seconds (startTime));
             sinkApp.Stop (Seconds (END_TIME));
 
-            startTime += poission_gen_interval (REQUEST_RATE);
+            startTime += poission_gen_interval (requestRate);
         }
     }
 }
@@ -123,9 +103,15 @@ int main (int argc, char *argv[])
     // Command line parameters parsing
 
     std::string runModeStr = "Conga";
+    unsigned randomSeed = 0;
+    std::string cdfFileName = "";
+    double load = 0.0;
 
     CommandLine cmd;
-    cmd.AddValue ("runMode", "Running Mode of this simulation: Conga, Conga-flow, ECMP", runModeStr);
+    cmd.AddValue ("runMode", "Running mode of this simulation: Conga, Conga-flow, Presto, ECMP", runModeStr);
+    cmd.AddValue ("randomSeed", "Random seed, 0 for random generated", randomSeed);
+    cmd.AddValue ("cdfFileName", "File name for flow distribution", cdfFileName);
+    cmd.AddValue ("load", "Load of the network, 0.0 - 1.0", load);
     cmd.Parse (argc, argv);
 
     RunMode runMode;
@@ -137,13 +123,23 @@ int main (int argc, char *argv[])
     {
         runMode = CONGA_FLOW;
     }
+    else if (runModeStr.compare ("Presto") == 0)
+    {
+        runMode = PRESTO;
+    }
     else if (runModeStr.compare ("ECMP") == 0)
     {
         runMode = ECMP;
     }
     else
     {
-        NS_LOG_ERROR ("The running mode should be Conga, Conga-flow and ECMP");
+        NS_LOG_ERROR ("The running mode should be Conga, Conga-flow, Presto and ECMP");
+        return 0;
+    }
+
+    if (load < 0.0 || load > 1.0)
+    {
+        NS_LOG_ERROR ("The network load should within 0.0 and 1.0");
         return 0;
     }
 
@@ -157,7 +153,23 @@ int main (int argc, char *argv[])
     Config::SetDefault ("ns3::TcpSocket::RcvBufSize", UintegerValue (100000000));
 
     NS_LOG_INFO ("Initialize random seed");
-    srand((unsigned)time (NULL));
+    if (randomSeed == 0)
+    {
+        srand ((unsigned)time (NULL));
+    }
+    else
+    {
+        srand (randomSeed);
+    }
+
+    NS_LOG_INFO ("Initialize CDF table");
+    struct cdf_table* cdfTable = new cdf_table ();
+    init_cdf (cdfTable);
+    load_cdf (cdfTable, cdfFileName.c_str ());
+
+    NS_LOG_INFO ("Calculating request rate");
+    double requestRate = load * SPINE_LEAF_CAPACITY  / avg_cdf (cdfTable);
+    NS_LOG_INFO ("Average request rate: " << requestRate << " per second");
 
     NS_LOG_INFO ("Create nodes");
 
@@ -189,10 +201,18 @@ int main (int argc, char *argv[])
     {
         Config::SetDefault ("ns3::Ipv4GlobalRouting::PerflowEcmpRouting", BooleanValue(true));
     }
-    internet.Install (leaf0);
-    internet.Install (leaf1);
+
     internet.Install (spine0);
     internet.Install (spine1);
+
+    // Enable DRB
+    if (runMode == PRESTO)
+    {
+        internet.SetDrb (true);
+    }
+
+    internet.Install (leaf0);
+    internet.Install (leaf1);
 
     NS_LOG_INFO ("Install channels and assign addresses");
 
@@ -200,9 +220,9 @@ int main (int argc, char *argv[])
     Ipv4AddressHelper ipv4;
 
     // Setting switches
-    p2p.SetDeviceAttribute ("DataRate", StringValue ("4Gbps"));
-    p2p.SetChannelAttribute ("Delay", TimeValue(MicroSeconds (100)));
-    p2p.SetQueue ("ns3::DropTailQueue", "MaxPackets", UintegerValue (100));
+    p2p.SetDeviceAttribute ("DataRate", DataRateValue (DataRate (SPINE_LEAF_CAPACITY)));
+    p2p.SetChannelAttribute ("Delay", TimeValue(LINK_LATENCY));
+    p2p.SetQueue ("ns3::DropTailQueue", "MaxPackets", UintegerValue (BUFFER_SIZE));
 
     NodeContainer leaf0_spine0_1 = NodeContainer (leaf0, spine0);
     NodeContainer leaf0_spine0_2 = NodeContainer (leaf0, spine0);
@@ -229,20 +249,20 @@ int main (int argc, char *argv[])
     NetDeviceContainer netdevice_leaf1_spine1_2 = p2p.Install (leaf1_spine1_2);
 
     ipv4.SetBase ("10.1.1.0", "255.255.255.0");
-    ipv4.Assign (netdevice_leaf0_spine0_1);
-    ipv4.Assign (netdevice_leaf0_spine0_2);
+    Ipv4InterfaceContainer addr_leaf0_spine0_1 = ipv4.Assign (netdevice_leaf0_spine0_1);
+    Ipv4InterfaceContainer addr_leaf0_spine0_2 = ipv4.Assign (netdevice_leaf0_spine0_2);
 
-    ipv4.Assign (netdevice_leaf0_spine1_1);
-    ipv4.Assign (netdevice_leaf0_spine1_2);
+    Ipv4InterfaceContainer addr_leaf0_spine1_1 = ipv4.Assign (netdevice_leaf0_spine1_1);
+    Ipv4InterfaceContainer addr_leaf0_spine1_2 = ipv4.Assign (netdevice_leaf0_spine1_2);
 
-    ipv4.Assign (netdevice_leaf1_spine0_1);
-    ipv4.Assign (netdevice_leaf1_spine0_2);
+    Ipv4InterfaceContainer addr_leaf1_spine0_1 = ipv4.Assign (netdevice_leaf1_spine0_1);
+    Ipv4InterfaceContainer addr_leaf1_spine0_2 = ipv4.Assign (netdevice_leaf1_spine0_2);
 
-    ipv4.Assign (netdevice_leaf1_spine1_1);
-    ipv4.Assign (netdevice_leaf1_spine1_2);
+    Ipv4InterfaceContainer addr_leaf1_spine1_1 = ipv4.Assign (netdevice_leaf1_spine1_1);
+    Ipv4InterfaceContainer addr_leaf1_spine1_2 = ipv4.Assign (netdevice_leaf1_spine1_2);
 
     // Setting servers under leaf 0
-    p2p.SetDeviceAttribute ("DataRate", StringValue ("1Gbps"));
+    p2p.SetDeviceAttribute ("DataRate", DataRateValue (DataRate (LEAF_SERVER_CAPACITY)));
 
     ipv4.SetBase ("10.1.2.0", "255.255.255.0");
     for (int i = 0; i < LEAF_NODE_COUNT; i++)
@@ -308,11 +328,28 @@ int main (int argc, char *argv[])
         }
     }
 
+    if (runMode == PRESTO)
+    {
+        NS_LOG_INFO ("Setting up DRB switch");
+        Ipv4DrbHelper drb;
+        Ptr<Ipv4Drb> drb0 = drb.GetIpv4Drb (leaf0->GetObject<Ipv4> ());
+        drb0->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf0_spine0_1.GetAddress (1));
+        drb0->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf0_spine0_2.GetAddress (1));
+        drb0->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf0_spine1_1.GetAddress (1));
+        drb0->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf0_spine1_2.GetAddress (1));
+
+        Ptr<Ipv4Drb> drb1 = drb.GetIpv4Drb (leaf1->GetObject<Ipv4> ());
+        drb1->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf1_spine0_1.GetAddress (1));
+        drb1->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf1_spine0_2.GetAddress (1));
+        drb1->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf1_spine1_1.GetAddress (1));
+        drb1->AddCoreSwitchAddress (PRESTO_RATIO, addr_leaf1_spine1_2.GetAddress (1));
+    }
+
     NS_LOG_INFO ("Create applications");
 
     // Install apps on servers under switch leaf0
-    install_applications(servers0, servers1, serversAddr1);
-    install_applications(servers1, servers0, serversAddr0);
+    install_applications(servers0, servers1, requestRate, cdfTable, serversAddr1);
+    install_applications(servers1, servers0, requestRate, cdfTable, serversAddr0);
 
     NS_LOG_INFO ("Enabling flow monitor");
 
@@ -335,10 +372,16 @@ int main (int argc, char *argv[])
     {
         fileName = "conga-flow-simulation.xml";
     }
+    else if (runMode == PRESTO)
+    {
+        fileName = "presto-simulation.xml";
+    }
     else if (runMode == ECMP)
     {
         fileName = "ecmp-simulation.xml";
     }
 
     flowMonitor->SerializeToXmlFile(fileName, true, true);
+
+    free_cdf (cdfTable);
 }
