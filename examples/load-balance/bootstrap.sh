1ddefcc05fe57f452ef46e7acb367fe608c494de
Junxue Zhang <snow.zjx@me.com>
2016-08-03 00:20:26 +0800

    Bug fix

--- examples/load-balance/bootstrap.sh
+++ examples/load-balance/bootstrap.sh
@@ -2,9 +2,9 @@
 
 cd ../..
 
-for load in 0.3, 0.5, 0.8
+for load in 0.3 0.5 0.8
 do
-        for run in 1, 2, 3 
+        for run in 1 2 3 
 	do
                  nohup ./waf --run "conga-simulation --runMode=Conga --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/conga-$load-$run.out 2>&1 &
 		 nohup ./waf --run "conga-simulation --runMode=ECMP --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/ecmp-$load-$run.out 2>&1 &
