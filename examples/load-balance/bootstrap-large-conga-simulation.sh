9d54c85cd4ddd420eaf2e60d822fd3d254f13646
Junxue Zhang <snow.zjx@me.com>
2016-08-04 01:35:51 +0800

    bug fix

--- examples/load-balance/bootstrap-large-conga-simulation.sh
+++ examples/load-balance/bootstrap-large-conga-simulation.sh
@@ -6,9 +6,9 @@ for load in 0.3 0.5 0.8
 do
         for run in 1
 	do
-                 nohup ./waf --run "conga-simulation --runMode=Conga --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/large-conga-$load-$run.out 2>&1 &
-                 nohup ./waf --run "conga-simulation --runMode=Conga-flow --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/large-conga-flow-$load-$run.out 2>&1 &
-		 nohup ./waf --run "conga-simulation --runMode=ECMP --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/large-ecmp-$load-$run.out 2>&1 &
+                 nohup ./waf --run "conga-simulation-large --runMode=Conga --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/large-conga-$load-$run.out 2>&1 &
+                 nohup ./waf --run "conga-simulation-large --runMode=Conga-flow --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/large-conga-flow-$load-$run.out 2>&1 &
+		 nohup ./waf --run "conga-simulation-large --runMode=ECMP --randomSeed=$run --cdfFileName=examples/load-balance/VL2_CDF.txt --load=$load" > /tmp/large-ecmp-$load-$run.out 2>&1 &
 	done
 done
 
